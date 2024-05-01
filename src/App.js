984d69849fcec00b5b6937aa8b1aec0c5d975570
Mert <mertparlak123@hotmail.com>
2024-05-01 13:48:59 +0300

    Refactor App.js to handle API key error and display error message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import MovieList from "./components/MovieList";
 import Main from "./components/Main";
 import { useEffect, useState } from "react";
 import Loader from "./components/Loader";
+import ErrorMessage from "./components/Error/ErrorMessage";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
@@ -13,30 +14,45 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(function () {
     async function fetchMovies() {
-      if (!API_KEY) {
-        console.error("API key is missing");
-        setIsLoading(true);
-        return <Loader />;
-      }
+      try {
+        if (!API_KEY) {
+          console.error("API key is missing");
+          setError("API key is missing");
+          error && <ErrorMessage message={error} />;
+          return;
+        }
 
-      setIsLoading(true);
-      const response = await fetch(`${API_KEY}&s=matrix`);
-      const data = await response.json();
-      setMovies(data.Search);
-      setIsLoading(false);
+        setIsLoading(true);
+        const response = await fetch(`${API_KEY}&s=matrix`);
+
+        if (!response.ok) throw new Error("Failed to fetch movies data");
+
+        const data = await response.json();
+        setMovies(data.Search);
+        setIsLoading(false);
+      } catch (err) {
+        console.error(err.message);
+        setError(err.message);
+        setIsLoading(false);
+      }
     }
     fetchMovies();
   }, []);
 
   return (
     <>
-      <NavBar movies={movies} />
+      <NavBar movies={movies}></NavBar>
 
       <Main>
-        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
+        <Box>
+          {isLoading && <Loader />}
+          {!isLoading && !error && <MovieList movies={movies} />}
+          {error && <ErrorMessage message={error} />}
+        </Box>
         <Box>
           <Summary watched={watched} />
           <MyMovieList watched={watched} />
